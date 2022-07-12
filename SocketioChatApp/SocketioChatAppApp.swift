a208d47fdfca82a9b4df2403b04a1c8ac6cf9e74
redshell0 <baseball0020@gmail.com>
2022-07-12 09:55:44 +0900

    Initial Commit

---
+++ SocketioChatApp/SocketioChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocketioChatAppApp.swift
+//  SocketioChatApp
+//
+//  Created by jinwoong Kim on 2022/07/12.
+//
+
+import SwiftUI
+
+@main
+struct SocketioChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
